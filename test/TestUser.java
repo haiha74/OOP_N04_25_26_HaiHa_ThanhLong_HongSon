82ece59355b96c162ec552a79d4a1d3b33f211c2
haiha74 <143710573+haiha74@users.noreply.github.com>
2025-09-08 09:53:09 +0000

    update User

---
+++ test/TestUser.java
@@ -0,0 +1,18 @@
+packet test;
+import User;
+
+public class TestUser {
+    public static void main(String[] args){
+        //Tao User
+        User u1 = new User("haiha12345", "12345678");
+        System.out.println(u1);
+
+        //Thay doi thong tin
+        u1.setUsername("haiha123");
+        u1.setPassword("1234567");
+
+        //In ra thong tin moi 
+        System.out.println("Username: " + u1.getUsername());
+        System.out.println("Password: " + u1.getPassword());
+    }
+}
