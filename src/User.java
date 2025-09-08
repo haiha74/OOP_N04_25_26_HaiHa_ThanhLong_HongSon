7afdfa794ebbd6b74c9433b8fee27f7addcd0ad1
haiha74 <143710573+haiha74@users.noreply.github.com>
2025-09-08 09:56:14 +0000

    forgot password

--- src/User.java
+++ src/User.java
@@ -1,24 +1,40 @@
 public class User {
-    private String username; //Ten dang nhap
-    private String password; //Mat khau
+    private String username; // Ten dang nhap
+    private String password; // Mat khau
 
     // Ham khoi tao
-    public User(String username, String password){
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
     // Getter/Setter cho usename
-    public String getUsername() { return username; }
-    public void setUsername(Sring username) { this.username = username;}
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(Sring username) {
+        this.username = username;
+    }
 
     // Getter/Setter cho password
-    public String getPassword() { return password; }
-    public void setPassword(Sring password) { this.password = password;}
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(Sring password) {
+        this.password = password;
+    }
 
     // In thoong tin
     @Override
-    public String toString(){
+    public String toString() {
         return "User{username='" + username + "', password='" + password + "'}";
+    }
+
+    public User forgotPasswordOrUsername(){
+
+        
+        return new User();
     }
 }
